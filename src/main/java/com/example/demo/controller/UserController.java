2e6962a2a5765534bddb8214563508a9c124fdb9
minyoungkim <minyoung_kim3@tmaxsoft.com>
2026-04-02 11:12:39 +0900

    add login api and get my info api

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,17 +1,35 @@
 package com.example.demo.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
+    public record Token(String accessToken, String refreshToken){}
     public record Me(String name, Integer age){};
-    
-    @GetMapping
-    public ResponseEntity<Me> me(){
-        return ResponseEntity.ok(new Me("minyoung",10));
+
+    public record LoginReq(String loginId, String password){};
+    public record AuthInfo(String accessToken){};
+
+    @PostMapping("/login")
+    public ResponseEntity<Token> login(@RequestBody LoginReq loginReq){
+        if(loginReq.loginId!=null) {
+            return ResponseEntity.ok(new Token("accessToken", "refreshToken"));
+        }
+        return ResponseEntity.badRequest().body(null);
     }
 
+    @PostMapping("/me/info")
+    public ResponseEntity<Me> me(@RequestBody AuthInfo authInfo){
+
+        if(authInfo.accessToken!=null) {
+            return ResponseEntity.ok(new Me("minyoung",10));
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+    }
 }
